0aca14dcbe95441e60063eddf4a24deb2da9f3e3
Aditya <ratnakeraditya04@gmail.com>
2023-02-09 17:06:49 +0530

    First Commit and Starter Code

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+
+// Components
+import Navigation from './components/Navigation'
+import Section from './components/Section'
+import Product from './components/Product'
+
+// ABIs
+import Etherbasket from './abis/Etherbasket.json'
+
+// Config
+import config from './config.json'
+
+function App() {
+
+  return (
+    <div>
+
+      <h2>Welcome to Etherbasket</h2>
+
+    </div>
+  );
+}
+
+export default App;
