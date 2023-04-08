1d5438d13477d45c72cd511347c069dae49c6447
Aditya <ratnakeraditya04@gmail.com>
2023-04-09 00:36:55 +0530

    Updated some basic functioning of contract and a testing function in JS

--- contracts/Etherbasket.sol
+++ contracts/Etherbasket.sol
@@ -2,6 +2,49 @@
 pragma solidity ^0.8.9;
 
 contract Etherbasket {
-    // Code Goes Here....
+    // Code Goes Here.....
     string public name = "Etherbasket";
+    address public owner ; 
+
+    struct Item {
+        uint256 id ; 
+        string name;
+        string category; 
+        string image;
+        uint256 cost; 
+        uint256 rating; 
+        uint256 stock;
+    }
+    // It;'s like a store house of different key value pairs.
+    mapping(uint256 => Item) public items ;
+    constructor() { 
+        name = "Etherbasket";
+        // Person who is deploying the smart contract will become the owner. 
+        owner = msg.sender; 
+    }
+
+//  Lists Products  
+    function list(uint256 _id, 
+            string memory _name, 
+            string memory _category, 
+            string memory _image,
+            uint256 _cost, 
+            uint256 _rating,
+            uint256 _stock 
+        ) public  {
+        // Code Goes Here....
+        // Create a object of that struct 
+        Item memory item = Item(_id,
+            _name,
+            _category,
+            _image,
+            _cost,
+            _rating,
+            _stock
+        );
+        // Save Items to BlockChain. AStore it in the mapping we created above.   
+        items[_id] = item ; 
+    }
+    
+
 } 
--- test/Etherbasket.js
+++ test/Etherbasket.js
@@ -1,13 +1,79 @@
 const { expect } = require("chai")
+const { ethers } = require("hardhat")
 
 const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 describe("Etherbasket", () => {
-  it('has a name', async () => {
+  let etherbasket
+  let deployer , buyer
+  beforeEach(async () => {
+
+    // SetUp Accounts 
+    [deployer,buyer]  = await ethers.getSigners()
+    console.log(deployer.address , buyer.address)
+    
+
+    // Deploy Contract Each time before each time a test is called. This will act as a hook. 
+    // Get Contract from the factory named Etherbasket.  
     const Etherbasket = await ethers.getContractFactory("Etherbasket");
+    // Deploy a copy of it. 
     etherbasket = await Etherbasket.deploy()
-    expect(await etherbasket.name()).to.equal("Etherbasket")
   })
-})
+
+
+
+  describe("Deployment" , () =>{
+    it('sets the owner', async () => {
+      // Fetch the address from it so as to check if it matches our provided value.
+          expect(await etherbasket.owner()).to.equal(deployer.address)
+        })
+    // Used only for testing and setting up our contracts folder purpose. 
+    it('has a name', async () => {
+    // Fetch the name from it so as to check if it matches our provided value.
+        expect(await etherbasket.name()).to.equal("Etherbasket")
+      })
+  })
+  
+
+
+
+
+
+
+  describe("Listing" , () =>{
+    let transaction 
+    const ID = 1 ;
+    const NAME = "Shoes" ; 
+    const CATEGORY = "Clothing"
+    const IMAGE = "https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/shoes.jpg";
+    // Cost of the Price Given here is expressed in the ether itself through token. 
+    const COST = tokens(1); 
+    const RATING = 4;
+    const STOCK = 5; 
+    beforeEach(async () => {
+        transaction = await etherbasket.connect(deployer).list (
+        ID, 
+        "Shoes",
+        "Clothing",
+        "IMAGE",
+        1,
+        4,
+        5
+
+        )
+    });
+    it('list the product', async () => {
+      // Fetch the address from it so as to check if it matches our provided value.
+          expect(await etherbasket.owner()).to.equal(deployer.address)
+        })
+    // Used only for testing and setting up our contracts folder purpose. 
+    it('has a name', async () => {
+    // Fetch the name from it so as to check if it matches our provided value.
+        expect(await etherbasket.name()).to.equal("Etherbasket")
+      })
+  })
+
+}) 
+ 
